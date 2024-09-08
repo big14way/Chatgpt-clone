703b55cec8b757fb0d65584c537a2e7ef4d1d922
GODSWILL <idolorwilly@icloud.com>
2024-09-08 03:56:11 +0100

    changes

---
+++ script.js
@@ -0,0 +1,10 @@
+const chatInput = document.querySelector("#chat-input");
+const sendButton = document.querySelector("#send-btn");
+
+let userText = null;
+
+const HandleOutgoingChat = () => {
+
+}
+
+sendButton.addEventListener("click", HandleOutgoingChat);
